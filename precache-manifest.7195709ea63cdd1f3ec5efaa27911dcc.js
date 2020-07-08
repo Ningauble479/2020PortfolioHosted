c0721020dbdcd1bf9acc2399442e8df6d85e37c6
Devon <Dvowen@cox.net>
2020-07-07 23:56:09 -0700

    Updates

---
+++ precache-manifest.7195709ea63cdd1f3ec5efaa27911dcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04bad8c4c2b3fe3d70b9c96571dd5009",
+    "url": "/2020PortfolioHosted/index.html"
+  },
+  {
+    "revision": "3ba981e4369984eff7d5",
+    "url": "/2020PortfolioHosted/static/css/main.d7446ac7.chunk.css"
+  },
+  {
+    "revision": "7443aca82b1b9b164f9c",
+    "url": "/2020PortfolioHosted/static/js/2.2ece10c6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/2020PortfolioHosted/static/js/2.2ece10c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ba981e4369984eff7d5",
+    "url": "/2020PortfolioHosted/static/js/main.f915a4b5.chunk.js"
+  },
+  {
+    "revision": "383fad2516eb396834c4",
+    "url": "/2020PortfolioHosted/static/js/runtime-main.35e657fd.js"
+  }
+]);
