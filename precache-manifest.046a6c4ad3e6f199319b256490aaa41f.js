a0f0eee98ffa905ef63d1976d4117d90dacb56f2
Devon <Dvowen@cox.net>
2020-09-01 22:33:01 -0700

    Updates

---
+++ precache-manifest.046a6c4ad3e6f199319b256490aaa41f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36e21cb3fdab26423219886dbea55707",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f86917a9e66f9660a7ec",
+    "url": "/static/css/main.cc2a0c7f.chunk.css"
+  },
+  {
+    "revision": "3f9473b9c03c8ef9cf7a",
+    "url": "/static/js/2.eedbd44d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.eedbd44d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f86917a9e66f9660a7ec",
+    "url": "/static/js/main.4b30fc58.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "bddc459ffc22e5dbb4d28860a1c5d0e0",
+    "url": "/static/media/DnDProject.bddc459f.png"
+  },
+  {
+    "revision": "cb2dac21e08369ffa16b00c1ea6f9b4d",
+    "url": "/static/media/RanchersBest.cb2dac21.png"
+  },
+  {
+    "revision": "d4d18a2ab37808ae3f9a033249e7674b",
+    "url": "/static/media/RoshiHealth.d4d18a2a.png"
+  },
+  {
+    "revision": "77758e9ad91e54c14371efa0d2364166",
+    "url": "/static/media/Tasky.77758e9a.png"
+  },
+  {
+    "revision": "7bfc30ecfd7273eadb968ee3bb28f4fb",
+    "url": "/static/media/profilePic.7bfc30ec.jpg"
+  },
+  {
+    "revision": "733a135b8d1422efa978522dd75f85f0",
+    "url": "/static/media/wp1828901-programmer-wallpapers.733a135b.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/2020portfoliohosted/precache-manifest.67ace3db2526e906f416b79da34380a2.js"
+  "/precache-manifest.046a6c4ad3e6f199319b256490aaa41f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/2020portfoliohosted/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
